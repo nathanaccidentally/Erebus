4af709484f5fe0d14cdca999edb80473716345dd
Nathan Goodwin <nathanaccidentally@gmail.com>
2017-06-23 16:16:00 -0500

    Twitter Update

--- erebusprefs/EREBUSRootListController.m
+++ erebusprefs/EREBUSRootListController.m
@@ -19,7 +19,7 @@
 }
 
 -(void)tweetMe {
-	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/clichewow"]];
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/nathangoodwinn"]];
 }
 
 @end
